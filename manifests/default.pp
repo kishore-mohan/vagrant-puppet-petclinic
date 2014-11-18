76db83c411591154181c681fb036ba5870ee6977
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2014-11-18 18:26:47 +0100

    cleanup

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,6 @@
 
-# class { 'petclinic': } 
+class { 'petclinic': 
+  petclinic_war_url => "https://github.com/cyrille-leclerc/spring-petclinic/releases/download/petclinic-1.0.0-clc/petclinic.war"
+} 
 
-include petclinic
+
