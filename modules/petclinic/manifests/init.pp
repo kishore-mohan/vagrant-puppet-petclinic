c2cb4655b21fbc05c3c09574ceed898f7e480c26
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2014-11-18 20:52:20 +0100

    force war reload even if url is "puppet:/// ..."

--- modules/petclinic/manifests/init.pp
+++ modules/petclinic/manifests/init.pp
@@ -34,6 +34,10 @@ class petclinic ($petclinic_war_url = "https://github.com/cyrille-leclerc/spring
     package_name        => 'tomcat',
     source_url => 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz'
   } ->
+  file { '/opt/apache-tomcat/webapps/petclinic.war':
+    # manually delete the war because puppetlabs-tomcat:v1.2.0 does not overwrite existing war if the source is "puppet:///..."
+    ensure => 'absent'
+  } ->
   tomcat::war { 'petclinic.war': 
     war_source => $petclinic_war_url
   } ->
