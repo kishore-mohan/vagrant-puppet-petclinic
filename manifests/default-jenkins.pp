a7cefddebe43048576dedb5c6a776c60d24b8ca3
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2014-11-19 13:45:05 +0100

    define the ip of jenkins.localdomain in Vagrant deployments

--- manifests/default-jenkins.pp
+++ manifests/default-jenkins.pp
@@ -1,9 +1,10 @@
-
+# define jenkins ip for puppet report in Vagrant
+host { 'jenkins.localdomain':
+    ip => '172.28.128.1',
+} ->
 class { 'petclinic': 
   petclinic_war_url => "puppet:///modules/petclinic/petclinic.war"
 } 
 
-host { 'jenkins.localdomain':
-    ip => '172.28.128.1',
-}
+
 
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,7 @@
-
+# define jenkins ip for puppet report in Vagrant
+host { 'jenkins.localdomain':
+    ip => '172.28.128.1',
+} ->
 class { 'petclinic': 
   petclinic_war_url => "https://github.com/cyrille-leclerc/spring-petclinic/releases/download/petclinic-1.0.0-clc/petclinic.war"
 } 
